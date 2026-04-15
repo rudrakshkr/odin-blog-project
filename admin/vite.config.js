0750c553577e1858400989c2d97c64578412bfac
Rudraksh Kumar <rudrakshkumar2908@gmail.com>
2026-04-15 19:50:18 +0530

    feat(frontend): loaded api url from .enve and used it in vite config

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,22 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-  server: {
-    port: 5174,
-    proxy: {
-      '/api': {
-        target: 'https://odin-blog-api-fbki.onrender.com/',
-        changeOrigin: true
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+
+    server: {
+      port: 5175,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL || 'http://localhost:3000', 
+          changeOrigin: true
+        }
       }
     }
   }
